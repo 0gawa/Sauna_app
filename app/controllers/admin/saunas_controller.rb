95ce21deaf7394365b7beb0643e8fa50a311c73c
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-04-19 18:50:51 +0900

    管理者側：サウナ施設一覧ページ

--- app/controllers/admin/saunas_controller.rb
+++ app/controllers/admin/saunas_controller.rb
@@ -1,10 +1,18 @@
 class Admin::SaunasController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_manager!
 
     def new
         @sauna = Sauna.new
         @sauna_infos = SaunaInfo.all
         @water = Water.all
+    end
+
+    def show
+        @sauna = Sauna.find(params[:id])
+    end
+
+    def index
+        @saunas= Sauna.all
     end
 
     def create
