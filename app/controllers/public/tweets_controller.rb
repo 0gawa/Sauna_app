cddced1e7e9ee2139080bcab013156914ac95912
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-08-10 14:58:27 +0900

    レイアウトの修正

--- app/controllers/public/tweets_controller.rb
+++ app/controllers/public/tweets_controller.rb
@@ -1,5 +1,5 @@
 class Public::TweetsController < ApplicationController
-    before_action :authenticate_user!, except: [:index]
+    before_action :authenticate_user!
 
     #サ活投稿一覧はログインしていなくても閲覧可能にすること
 
