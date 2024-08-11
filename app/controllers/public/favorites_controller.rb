05c5277d9e13991e1063c798002d75c83c659f02
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-08-12 00:04:25 +0900

    turbo導入とバグの修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -5,11 +5,13 @@ class Public::FavoritesController < ApplicationController
         tweet = Tweet.find(params[:tweet_id])
         favorite = current_user.favorites.new(tweet_id: tweet.id)
         favorite.save
+        render turbo_stream: turbo_stream.update("tweet_#{tweet.id}_favorite", partial: 'public/favorites/btn_tweet', locals: {tweet: tweet})
     end
   
     def destroy
         tweet = Tweet.find(params[:tweet_id])
         favorite = current_user.favorites.find_by(tweet_id: tweet.id)
         favorite.destroy
+        render turbo_stream: turbo_stream.update("tweet_#{tweet.id}_favorite", partial: 'public/favorites/btn_tweet', locals: {tweet: tweet})
     end
 end
