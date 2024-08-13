a9ed1104fc42f21ea1e7244ddebfc032b0628575
0gawa <129659037+0gawa@users.noreply.github.com>
2024-08-13 19:30:18 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,6 @@
-10.times do |n|
-    Sauna.create!(
-      name: "サウナ施設 No.#{n + 1}",
-      address: "東京都#{n + 1}丁目"
-    )
-    SaunaInfo.create!(
-        temperature: n+90,
-        sauna_id: n+1
-    )
-    Water.create!(
-        temperature: n+10,
-        sauna_id: n+1
-    )
-end
-
-10.times do |n|
-    SaunaInfo.create!(
-        temperature: n+100,
-        sauna_id: n+1
-    )
-    Water.create!(
-        temperature: n+14,
-        sauna_id: n+1
-    )
-end
-
-
 Manager.create!(
-    email: "2024kanrisya@hitori-times.com",
-    name: "管理者hitori",
-    encrypted_password: "aD3~yA+kwpVe",
-    password: "aD3~yA+kwpVe"
+    email: "1111@1111",
+    name: "kanri",
+    encrypted_password: "kanri",
+    password: "kanri"
 )
