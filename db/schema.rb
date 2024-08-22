770d045e44b370333b5946d657b8e23b4746f410
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-08-23 00:18:42 +0900

    重要：DBの最適化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_14_132557) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_25_142546) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,34 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_14_132557) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "phone_number"
-    t.string "subject", null: false
-    t.string "content", null: false
-    t.string "prefecture"
-    t.string "address"
-    t.integer "is_read", default: 0, null: false
-    t.integer "user_id", null: false
-    t.integer "type", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -185,7 +157,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_14_132557) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "favorites", "tweets"
   add_foreign_key "favorites", "users"
-
   add_foreign_key "sauna_infos", "saunas"
   add_foreign_key "tweets", "saunas"
   add_foreign_key "tweets", "users"
