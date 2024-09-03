8fde93ad8d0883749a0405925dda1f5c29036463
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-09-03 22:07:18 +0900

    テストケース

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 5.0.0'
   gem 'factory_bot_rails'
   gem 'capybara'
+  gem "faker"
 end
 
 group :development do
--- spec/controllers/public_users_controller_spec.rb
+++ spec/controllers/public_users_controller_spec.rb
@@ -2,24 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Public::UsersController, type: :controller do
     describe "ユーザーコントローラーに関して" do
-        before do
-            @user = User.create!(name: "test", email: "1111@gmail.com", password: "111111")
-            sign_up(@user)
+        #ユーザーが保存されていたらすべて削除する
+        if User.all.count >= 0
+            User.all.each do |user|
+                user.destroy
+            end
         end
-    
+        #ユーザー１、２を作成する
+        let(:user) { create(:user) }
+        let!(:other_user) { create(:user) }
+
+        before do
+            sign_in user
+        end
+        
+        it "ログインできる" do
+            visit user_path(user.id)
+            expect(page).to have_content 'マイページ'
+        end
+        
+        context "ページが正しく表示されるか" do
+            it "mypage：マイページ" do
+                visit user_path(user.id)
+                expect(page).to have_content 'マイページ'
+            end
+        end
+
         context "ユーザー編集ができる" do
             it "名前を変更する" do
-                visit edit_user_path(@user.id)
+                visit edit_user_path(user.id)
                 fill_in 'user[name]', with: "test(edited)"
                 click_button '保存する'
                 expect(@user.name).to eq "test(edited)"
             end
         end
 
-        context "ページが正しく表示されるか" do
-            it "mypage：マイページ" do
-                
-            end
-        end
     end
 end
